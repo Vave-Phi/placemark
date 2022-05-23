ad3624382a7f191442ab592f80d625994b9d2b56
Philipp Bachfischer <philipp.bachfischer@recrewt.de>
2022-05-23 20:48:10 +0200

    PM-6: *enabled cors *filter for get all pois *validate is admin endpoint

--- src/server.js
+++ src/server.js
@@ -25,8 +25,9 @@ const swaggerOptions = {
 
 async function init() {
   const server = Hapi.server({
-    port: 3000,
+    port: 3001,
     host: "localhost",
+    routes: { cors: true },
   });
   await server.register(Vision);
   await server.register(Inert);
